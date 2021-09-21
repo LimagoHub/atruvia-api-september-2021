71537871e9cbc48b0706e98032d0cf39a2dc56ce
Jo Wagner <jowagner@limago.de>
2021-09-21 10:59:51 +0200

    initial commit

---
+++ frontend/src/main/java/de/atruvia/kreditantrag/frontend/KreditAntragErfassenServiceApplication.java
@@ -0,0 +1,13 @@
+package de.atruvia.kreditantrag.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KreditAntragErfassenServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KreditAntragErfassenServiceApplication.class, args);
+	}
+
+}
